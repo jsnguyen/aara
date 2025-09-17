7de20ed4e55c705725436665f8f0539ff4bc157d
Jayke Nguyen <jsn.phpro1701@gmail.com>
2025-09-17 11:43:16 -1000

    added asynchronous saving, config file support, and state saving so you know what files you've saved

--- src/aara/aara.py
+++ src/aara/aara.py
@@ -1,6 +1,10 @@
+from pathlib import Path
 import argparse
-
+from configparser import ConfigParser
+import re
 import urllib.request
+import aiohttp
+
 from bs4 import BeautifulSoup
 
 from textual.app import App, ComposeResult
@@ -16,34 +20,34 @@ def get_arxiv_articles(category):
 
     soup = BeautifulSoup(response_text, "html.parser")
 
-    date_content = soup.find('h3', string=lambda s: s and "Showing new listings for" in s)
+    date_content = soup.find("h3", string=lambda s: s and "Showing new listings for" in s)
     date_str = date_content.text.strip()[len("Showing new listings for "):] if date_content else "Date header not found."
 
-    new_articles = soup.find_all('dl')[0]
+    new_articles = soup.find_all("dl")[0]
 
-    dts = new_articles.find_all('dt')
-    dds = new_articles.find_all('dd')
+    dts = new_articles.find_all("dt")
+    dds = new_articles.find_all("dd")
 
     articles = []
 
     for dt, dd in zip(dts, dds):
-        arxiv_id = dt.find('a', title='Abstract')
-        arxiv_id = arxiv_id.text.strip().split(':')[1] if arxiv_id else "N/A"
+        arxiv_id = dt.find("a", title="Abstract")
+        arxiv_id = arxiv_id.text.strip().split(":")[1] if arxiv_id else "N/A"
 
-        title = dd.find('div', class_='list-title mathjax')
-        title = title.text.replace('Title:', '').strip() if title else "N/A"
+        title = dd.find("div", class_="list-title mathjax")
+        title = title.text.replace("Title:", "").strip() if title else "N/A"
 
-        authors = dd.find('div', class_='list-authors')
-        authors = authors.text.replace('Authors:', '').strip() if authors else "N/A"
+        authors = dd.find("div", class_="list-authors")
+        authors = authors.text.replace("Authors:", "").strip() if authors else "N/A"
 
-        abstract = dd.find('p', class_='mathjax')
+        abstract = dd.find("p", class_="mathjax")
         abstract = abstract.text.strip() if abstract else "N/A"
 
-        comments = dd.find('div', class_='list-comments')
-        comments = comments.text.replace('Comments:', '').strip() if comments else ""
+        comments = dd.find("div", class_="list-comments")
+        comments = comments.text.replace("Comments:", "").strip() if comments else ""
 
-        subjects = dd.find('div', class_='list-subjects')
-        subjects = subjects.text.replace('Subjects:', '').strip() if subjects else ""
+        subjects = dd.find("div", class_="list-subjects")
+        subjects = subjects.text.replace("Subjects:", "").strip() if subjects else ""
 
         articles.append({
             "arxiv_id": arxiv_id,
@@ -78,31 +82,93 @@ class AARA(App):
     """
 
     BINDINGS = [
-                ("q", "quit", "Quit"),
+                ("q", "quit()", "Quit"),
                 ("{", "jump_five_up()", "Jump Five Up"),
                 ("}", "jump_five_down()", "Jump Five Down"),
                 ("g", "jump_top()", "Jump to Top"),
                 ("G", "jump_bottom()", "Jump to Bottom"),
                 ("a", "open_url('abs')", "Open Abstract"),
-                ("s", "open_url('pdf')", "Open PDF")
+                ("s", "save_url()", "Save PDF"),
+                ("d", "open_url('pdf')", "Open PDF")
                ]
 
-    def __init__(self, category):
+    DEFAULT_CONFIG = {"CONFIG":
+                        {"num_words": 4},
+                      "FILEPATHS":
+                        {"download_folder": Path.home() / "Downloads"}}
+
+    def __init__(self, category, config_path=None):
         super().__init__()
         self.category = category
         self.date_str, self.articles = get_arxiv_articles(self.category)
 
+        if config_path is None:
+            self.config_folder = Path.home() / ".config/aara"
+        else:
+            self.config_folder = Path(config_path)
+
+        self.config_path = self.config_folder / "aara.ini"
+        self.saved_state_path = self.config_folder / "state.ini"
+
+        if self.config_path.exists():
+            self.load_config()
+        else:
+            self.make_default_config()
+            self.load_config()
+
+        if self.saved_state_path.exists():
+            self.load_saved()
+        else:
+            self.saved = {self.date_str: []}
+
+    def make_default_config(self):
+        config = ConfigParser()
+        config.read_dict(self.DEFAULT_CONFIG)
+        config_folder = self.config_path.parent
+        config_folder.mkdir(parents=True, exist_ok=True)
+        with open(self.config_path, "w") as configfile:
+            config.write(configfile)
+
+    def load_config(self):
+        config = ConfigParser()
+        config.read(self.config_path)
+        self.num_words = int(config["CONFIG"]["num_words"])
+        self.download_folder = Path(config["FILEPATHS"]["download_folder"])
+        if not self.download_folder.exists():
+            self.download_folder.mkdir(parents=True, exist_ok=True)
+
+    def load_saved(self):
+        config = ConfigParser()
+        config.read(self.saved_state_path)
+        self.saved = {}
+        for date in config.sections():
+            self.saved[date] = config[date]["saved_ids"].split(",")
+
+    def save_saved(self):
+        config = ConfigParser()
+        for date, ids in self.saved.items():
+            config[date] = {"saved_ids": ",".join(ids).strip(",")}
+        with open(self.saved_state_path, "w") as configfile:
+            config.write(configfile)
+
     def compose(self) -> ComposeResult:
         yield Header()
+
+        self.status_bar = Static("Status", id="status_bar")
+        yield self.status_bar
 
         with VerticalGroup():
             yield Static(f"[dim]{self.category} | {self.date_str} | Total Articles: {len(self.articles)}[/dim]")
         with Horizontal():
 
-            self.list_view = ListView(*[
-                ListItem(Static(f"[dim][{i+1:>2}][/dim] [b]{article['title']}[/b]"))
-                for i, article in enumerate(self.articles)
-            ])
+            titles = []
+            for i, article in enumerate(self.articles):
+                title = f"[dim][{i+1:>2}][/dim] [b]{article['title']}[/b]"
+                if article["arxiv_id"] in self.saved.get(self.date_str, []):
+                    title = f"✓ {title}"
+                titles.append(title)
+
+            self.list_view = ListView(*[ListItem(Static(title)) for title in titles])
             yield self.list_view
 
             self.abstract_view = VerticalScroll(Static("Select an article to view its abstract."), id="abstract")
@@ -114,6 +180,10 @@ class AARA(App):
         self.list_view.focus()
         if self.articles:
             self.show_abstract(0)
+
+    def action_quit(self):
+        self.save_saved()
+        self.exit()
 
     def show_abstract(self, idx):
         info = self.articles[idx]
@@ -135,9 +205,55 @@ class AARA(App):
         self.show_abstract(self.list_view.index)
 
     def action_open_url(self, urltype):
-        arxiv_id = self.articles[self.list_view.index]['arxiv_id']
+        arxiv_id = self.articles[self.list_view.index]["arxiv_id"]
         arxiv_url = f"https://arxiv.org/{urltype}/{arxiv_id}"
         self.open_url(arxiv_url)
+
+    def action_save_url(self):
+        arxiv_id = self.articles[self.list_view.index]["arxiv_id"]
+        self.saved[self.date_str].append(arxiv_id)
+
+        # might accidentally get the institutional name, not sure how to fix this
+        first_author_lastname = self.articles[self.list_view.index]["authors"].split(",")[0].split()[-1]
+
+        shortened_title = "_".join(self.articles[self.list_view.index]["title"].split()[:self.num_words])
+        rejected_chars = ["/", "\\", ":", "*", "?", "\"", "<", ">", "|", "$"]
+        shortened_title = "".join(c for c in shortened_title if c not in rejected_chars)
+        shortened_title = re.sub(r"[^a-zA-Z0-9]", "_", shortened_title).strip("_")
+
+        year = self.date_str.split()[-1]
+
+        arxiv_url = f"https://arxiv.org/pdf/{arxiv_id}"
+        save_path = self.download_folder / f"{arxiv_id}_{year}_{first_author_lastname}_{shortened_title}.pdf"
+
+        home_str = str(Path.home())
+        save_path_str = str(save_path)
+        
+        if save_path_str.startswith(home_str):
+           save_path_str = save_path_str.replace(home_str, "~", 1)
+
+        if save_path.exists():
+            self.status_bar.update(f"File already exists, at {save_path_str}")
+            return
+
+        self.status_bar.update(f"Saving to {save_path_str}")
+
+        current_item = self.list_view.highlighted_child
+        if current_item:
+            #current_text = self.articles[self.list_view.index]["title"]
+            current_text = current_item.children[0].render().plain
+            if not current_text.startswith("✓"):
+                new_text = f"✓ {current_text}"
+                current_item.children[0].update(new_text)
+
+        self.run_worker(self.save_pdf_url(arxiv_url, save_path))
+
+    async def save_pdf_url(self, url, save_path):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                with open(save_path, "wb") as outfile:
+                    async for chunk in response.content.iter_chunked(8192):
+                        outfile.write(chunk)
 
     def action_jump_five_up(self):
         for _ in range(5):
